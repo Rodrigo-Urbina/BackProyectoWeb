ba941f97cbe647686bd50484aeaa1ac803cdbb59
acevedodavid <david@acevedo.com>
2021-03-09 15:19:18 -0600

    JWT authentication working to retrieve content

--- node-app/router.js
+++ node-app/router.js
@@ -14,6 +14,25 @@ function generateAccessToken(user) {
   return jwt.sign(user, process.env.JWT_SECRET, { expiresIn: '604800' });
 }
 
+function authenticateToken(req, res, next) {
+  // Gather the jwt access token from the request header
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
+  if (token == null) {
+    return res.sendStatus(401); // if there isn't any token
+  }
+
+  jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+    if (err) return res.sendStatus(403); // Forbidden
+    req.email = decoded.email
+    next() // pass the execution off to whatever request the client intended
+  });
+}
+
+function userBoard(req, res) {
+  res.status(200).send('User content for ' + req.email);
+}
+
 const router = express.Router();
 router.use(express.urlencoded({ extended: false }));
 
@@ -80,5 +99,7 @@ router.post('/auth/signin', (req,res) => {
     }
   });
 });
+
+router.get('/test/user', [authenticateToken], userBoard);
 
 module.exports = router;
