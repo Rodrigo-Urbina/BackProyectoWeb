58a01e95be3287f44d4a4a162eac8fe21f435f7b
acevedodavid <david@acevedo.com>
2021-04-14 01:32:39 -0500

    Architecture updated to Model-Service-Controller
    Model - Data Access Layer
    - Connection to the database
    Service Layer
    -Business logic, reusable components
    Controller layer
    -Route handler functions

--- node-app/controllers/user.js
+++ node-app/controllers/user.js
@@ -4,102 +4,74 @@ const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 var db = require("../db");
 const { generateToken } = require("../services/authJWT");
+const userService = require("../services/user");
 
-exports.create = async function(req, res) {
-  db.query(
-    "INSERT INTO users SET ?",
-    { // TODO cambiar esto para que al recibirlo deba tener esos nombres y usar body nomas
-      email: req.body.email,
-      password: req.body.password,
-      name_first: req.body.firstName,
-      name_last: req.body.lastName
-    },
-    function (error, results, fields) {
-      if (error) {
-        if (error.errno == 1062) return res.status(409).send("User already exists");
-        return res.status(500).send("Internal Server Error");
-      }
-      return res.status(200).send("User was registered succesfully");
-    }
-  );
+exports.create = async function(req, res, next) {
+  await userService.create(req, res, next);
+  res.status(200).send("User was created succesfully");
 }
 
-exports.read = async function(req, res) {
-  var columns = "id, email, name_first, name_last, confirmed, blocked, role, phone";
-  var sql = "SELECT " + columns + " FROM users";
-
-  if (Object.keys(req.query).length != 0) { // if query is not empty
-    sql += " WHERE ?"; // add where clause for query
+exports.find = async function(req, res, next) {
+  let users;
+  if (req.query) {
+    users = await userService.find(req.query, next);
+  } else {
+    users = await userService.find({}, next);
   }
 
-  db.query(
-    sql,
-    req.query, // if query is empty it won't even be used because of missing WHERE clause
-    function (error, results, fields) {
-      if (error) {
-        return res.status(500).send("Internal Server Error");
-      }
-      return res.status(200).send(results);
-    }
-  );
+  //var columns = "id, email, name_first, name_last, confirmed, blocked, role, phone";
+  return res.status(200).send(users);
 }
 
-exports.update = async function(req, res) {
+exports.findOne = async function(req, res, next) {
+  let params = (req.query) ? req.query : {};
+  params.id = req.params.id;
+  let user = await userService.findOne(params, next);
+
+  //var columns = "id, email, name_first, name_last, confirmed, blocked, role, phone";
+
+  if (user) {
+    return res.status(200).send(user);
+  } else {
+    next({status: 404, message:'User not found'});
+  }
+}
+
+exports.update = async function(req, res, next) {
   if(Object.keys(req.body).length === 0) {
-    return res.status(422).send("Request body is empty")
+    next({status: 422, message:'Request body is empty'});
   }
-  db.query(
-    "UPDATE users SET ? WHERE id = ?",
-    [req.body, req.params.id],
-    function (error, results, fields) {
-      if (error) {
-        return res.status(500).send("Internal Server Error");
-      }
-      return res.status(200).send("User updated succesfully");
-    }
-  )
+
+  await userService.update(req.body, req.params.id, next);
+
+  res.status(200).send("User updated succesfully");
 }
 
-exports.delete = async function(req, res) {
-  db.query(
-    "DELETE FROM users WHERE id = ?",
-    [req.params.id],
-    function (error, results, fields) {
-      if (error) {
-        return res.status(500).send("Internal Server Error");
-      }
-      if (results.affectedRows == 0) {
-        return res.status(400).send("User not found");
-      }
-      return res.status(200).send("User deleted succesfully")
-    }
-  );
+exports.delete = async function(req, res, next) {
+
+  await userService.delete(req.params.id, next);
+
+  res.status(200).send("User deleted succesfully");
 }
 
-exports.signin = async function(req, res) {
-  db.query(
-    "SELECT * FROM users WHERE email = ?",
-    [req.body.email],
-    function (error, results, fields) {
-      if (error) {
-        return res.status(500).send("Internal Server Error");
-      }
-      if (results.length == 0) {
-        return res.status(401).send("Unauthorized");
-      } else {
-        let user = results[0];
-        bcrypt.compare(req.body.password, user.password, function (err, result) {
-          if (result && user.confirmed && !user.blocked) {
-            return res.status(200).send({
-              jwt: generateToken({
-                email: user.email
-              })
-            });
-          } else {
-            return res.status(401).send("Unauthorized");
-          }
-        });
-      }
+
+exports.signin = async function(req, res, next) {
+    req.query = {email: req.body.email};
+    let user = await userService.findOne(req, res, next);
+
+    if (user) {
+      bcrypt.compare(req.body.password, user.password, function (err, result) {
+        if (result && user.confirmed && !user.blocked) {
+          return res.status(200).send({
+            jwt: generateToken({
+              email: user.email
+            })
+          });
+        } else {
+          next({status: 401, message:'Unauthorized'});
+        }
+      });
+    } else {
+      next({status: 401, message:'Unauthorized'});
     }
-  );
 }
